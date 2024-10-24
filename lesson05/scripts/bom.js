e27778b920ba972ba4c4df0ebfecef453a806c4a
JulieHirschi <juliehirschi04@gmail.com>
2024-10-24 12:54:33 -0600

    book

--- lesson05/scripts/bom.js
+++ lesson05/scripts/bom.js
@@ -2,15 +2,16 @@ const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
-button.addEventListener('click', function () {
+button.addEventListener('click', () => {
     if (input.value != '') {
         const li = document.createElement('li');
         const deleteButton = document.createElement('button');
-        li.textContent = input.value;
+        const link = `https://www.churchofjesuschrist.org/search?facet=scriptures&lang=eng&query=${input.value}&page=1`
+        li.innerHTML = `<a href=${link}>${input.value}</a`;
         deleteButton.textContent = '❌';
         li.append(deleteButton);
         list.append(li);
-        deleteButton.addEventListener('click', function () {
+        deleteButton.addEventListener('click', () => {
             list.removeChild(li);
         });
         input.value = '';
