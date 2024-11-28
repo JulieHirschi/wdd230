954d7ea1f0532c96be45530a072994413021d66c
JulieHirschi <juliehirschi04@gmail.com>
2024-11-28 01:54:03 -0700

    myNav

--- scripts/links.js
+++ scripts/links.js
@@ -10,32 +10,35 @@ async function getLinks() {
 sessions = document.getElementById("session");
 
 function displayLinks(weeks) {
+    myNav = document.createElement("div");
+    myNav.id = "myNav";
+
     weeks.forEach(week => {
         const list = document.createElement("li");
-        if (week.links.length === 1) {
-            const activity = week.links[0];
-            list.innerHTML = `<a href="${activity.url}">Week ${week.lesson}: ${activity.title}</a>`;
-        } else if (week.links.length > 1) {
-            const weekMultiple = document.createElement("span");
-            weekMultiple.textContent = `Week ${week.lesson}: `;
-            list.appendChild(weekMultiple);
-            weekMultiple.style.color = "black";
+        // if (week.links.length === 1) {
+        //     const activity = week.links[0];
+        //     list.innerHTML = `<a href="${activity.url}">Week ${week.lesson}: ${activity.title}</a>`;
+        // } else if (week.links.length > 1) {
+        const weekMultiple = document.createElement("span");
+        weekMultiple.textContent = `Week ${week.lesson}: `;
+        list.appendChild(weekMultiple);
+        weekMultiple.style.color = "black";
 
-            week.links.forEach((activity, index) => {
-                const anchor = document.createElement("a");
-                anchor.href = `${activity.url}`;
-                anchor.textContent = activity.title;
-                list.appendChild(anchor);
+        week.links.forEach((activity, index) => {
+            const anchor = document.createElement("a");
+            anchor.href = `${activity.url}`;
+            anchor.textContent = activity.title;
+            list.appendChild(anchor);
 
-                if (index < week.links.length - 1) {
-                    const bar = document.createElement("span");
-                    bar.textContent = "|";
-                    bar.style.color = "black";
-                    list.appendChild(bar);
-                }
-            });
-            list.classList.add("multiple");
-        }
+            if (index < week.links.length - 1) {
+                const bar = document.createElement("span");
+                bar.textContent = "|";
+                bar.style.color = "black";
+                list.appendChild(bar);
+            }
+        });
+        list.classList.add("multiple");
+        // }
         sessions.appendChild(list);
     });
 }
