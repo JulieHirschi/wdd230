033806fd5c3e9acba57cd73ab5bdfd0c928e7d02
JulieHirschi <juliehirschi04@gmail.com>
2024-12-01 04:04:22 -0700

    pages

--- chamber/scripts/dark.js
+++ chamber/scripts/dark.js
@@ -2,7 +2,8 @@ const modeButton = document.querySelector("#mode");
 const body = document.querySelector("body");
 const main = document.querySelector("main");
 
-modeButton.addEventListener("click", () => {
+modeButton.addEventListener("click", (e) => {
+    console.log("clicked");
     body.classList.toggle('dark-mode');
     main.classList.toggle('dark-mode');
 });
